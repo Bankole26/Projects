1a3236bf08fc521eda867268d1eebbe89a5e18ff
darylshy <darylshy@yahoo.com>
2019-01-02 15:54:26 -0500

    adding changes to repo

--- To Do List/tdl.js
+++ To Do List/tdl.js
@@ -1,29 +1,32 @@
 $(document).ready(function() {
-    $('#add').click(function(){
-        let newListItem = $("#addtoList").val();
-        let listItem="<li><input type='checkbox'" +" name='todo-item-done'" +" class='todo-item-done'"+ 
-                         " value='" + newListItem + "' /> " + 
-                         newListItem +
-                         " <button id='deleteItem'>"+
-                         "Delete</button>"+" <button class='edit'>"+
-                         "Edit</button>" +"</li>";
-        if (newListItem.length>0){
-            $("#todoList").append(listItem);
-             
-            $("#addtoList").val('');
-        }
-    });
-    
-    $('#removeAll').click(function(){
-        $('li').remove();
-    });
-    
-   $('#deleteItem').on('click',function(){
-    $(this).parent().remove();
-});
+  $('#add').click(function() {
+    let newListItem = $('#addtoList').val();
+    let listItem = `<li class='todo-list-item'>
+        <input type="checkbox" name="todo-item-done" class="todo-item-done" value="${newListItem}"/> 
+        ${newListItem} 
+        <button class='deleteItem'>Delete</button>
+        <button class='edit'>Edit</button>
+    </li>`;
 
-    
-    $('#edit').click(function(){
-    $(this).parent().attr('contenteditable','true');
-    })
+    if (newListItem.length > 0) {
+      $('.todoList').append(listItem);
+      $('#addtoList').val('');
+    }
+  });
+
+  $('#removeAll').click(function() {
+    $('li').remove();
+  });
+
+  $('.todoList').on('click', 'li.todo-list-item', function(e) {
+    if (e.target.className === 'deleteItem') {
+      $(this).remove();
+    }
+  });
+
+  $('#edit').click(function() {
+    $(this)
+      .parent()
+      .attr('contenteditable', 'true');
+  });
 });
