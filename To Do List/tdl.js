b5970e34d83b03f605895f2ca29fb8c3965effb2
Bankole26 <45578518+Bankole26@users.noreply.github.com>
2019-01-07 16:32:45 -0500

    Add files via upload

--- To Do List/tdl.js
+++ To Do List/tdl.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
   $('#add').click(function() {
+    const d = new Date();
+    const u= d.toLocaleString();
+    
+    
+    
     let newListItem = $('#addtoList').val();
-    let listItem = `<li class='todo-list-item'>
-        <input type="checkbox" name="todo-item-done" class="done" value="${newListItem}"/> 
-        ${newListItem} 
-        <button class='deleteItem'>Delete</button>
-        <button class='edit'>Edit</button>
-    </li>`;
-
+            let listItem = `<div class="omni"><div class="toast-header">
+            <img src="..." class="rounded mr-2" alt=${i}>
+            <strong class="mr-auto">Task</strong>
+            <small value="${u}"/> 
+                ${u}</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="toast-body">
+            <li class="todo-list-item 
+        toast" role="alert" aria-live="assertive" aria-atomic="true">
+                <input type="checkbox" name="todo-item-done" class="done" value="${newListItem}"/> 
+                ${newListItem}
+                <button class='edit btn btn-success"'>Edit</button>
+            </li>
+          </div>
+            </div>`;
+    
+   // const x=document.getElementbyID("task");
+     // x.style.display = "block";
+     
+    
     if (newListItem.length > 0) {
       $('.todoList').append(listItem);
       $('#addtoList').val('');
@@ -18,10 +39,9 @@ $(document).ready(function() {
     $('li').remove();
   });
 
-  $('.todoList').on('click', 'li.todo-list-item', function(e) {
-    if (e.target.className === 'deleteItem') {
-      $(this).remove();
-    }
+  $('.todoList').on('click', '.ml-2.mb-1.close', function(e) {
+  $(".omni").fadeOut('slow', function() { 
+  $(".omni").remove();
   });
     
     $('.todoList').on('click', 'li.todo-list-item', function(e) {
@@ -29,6 +49,8 @@ $(document).ready(function() {
         $(this).toggleClass("strike");
     }
 });
+      
+  });
 
 
   $('.todoList').on('click', 'li.todo-list-item', function(e) {
@@ -37,5 +59,6 @@ $(document).ready(function() {
       .parent()
       .attr('contenteditable', 'true');
     }
+      
   });
 });
